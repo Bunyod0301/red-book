1ec68c6d4ee552a69479bcd48cdf9bf119aafdcd
Bunyod0301 <bunyodyusupov0@gmail.com>
2024-02-26 15:19:01 +0500

    login page add

--- src/components/Plants/PlantsComponent.tsx
+++ src/components/Plants/PlantsComponent.tsx
@@ -2,8 +2,58 @@ import React from 'react';
 
 const PlantsComponent = () => {
   return (
-    <div>
-      Plants
+    <div className='md:container md:mx-auto my-10'>
+      <div className='flex flex-wrap gap-4 my-4'>
+        <label className="input input-bordered flex items-center gap-2 grow">
+          <input type="text" className="grow" placeholder="Search" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+        </label>
+        <select className="select select-primary grow">
+          <option disabled selected>What is the best TV show?</option>
+          <option>Game of Thrones</option>
+          <option>Lost</option>
+          <option>Breaking Bad</option>
+          <option>Walking Dead</option>
+        </select>
+      </div>
+      <div className='flex flex-wrap gap-4'>
+        <div className="card w-80 glass grow">
+          <figure>
+            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/>
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-80 glass grow">
+          <figure>
+            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/>
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-80 glass grow">
+          <figure>
+            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/>
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
