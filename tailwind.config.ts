59d84e68afacd930c27f0a7b526182d372472234
Bunyod0301 <bunyodyusupov0@gmail.com>
2024-02-26 15:49:25 +0500

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,36 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      // backgroundImage: {
-      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      //   "gradient-conic":
-      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      // },
-      backgroundImage: (theme) => ({
-        // primary_gradient: `linear-gradient(to right, ${theme("colors.gradient1")}, ${theme("colors.gradient2")})`,
-        card_gradient1: "linear-gradient(180deg, color1 0%, color2 100%)",
-        text_gradient2: "linear-gradient(90.12deg, color4 0.05%, color3 70.33%);",
-      }),
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
     },
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: [
-      "light",
-      "dark",
-      "retro",
-      "valentine",
-      "aqua",
-      {
-        "custom-theme": {
-          primary: "#a991f7",
-          secondary: "#f6d860",
-          accent: "#37cdbe",
-          neutral: "#3d4451",
-          "base -100": "#ffffff",
-        },
-      },
-    ]
-  }
+  plugins: [require("tailwindcss"),
+    require("daisyui"),]
 };
 export default config;
